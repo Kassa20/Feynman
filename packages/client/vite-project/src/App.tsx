e4948e97d59aad7a8ad499d366a3ae98a0780bdb
ykassa <ykassa47@gmail.com>
2026-07-21 12:02:36 -0700

    setup vercel and client

---
+++ packages/client/vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p>Client</p>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import type { Request, Response } from 'express';
+import { openai } from '@ai-sdk/openai';
+import { generateText } from 'ai';
+
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("server online!")
+})
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+    const { text } = await generateText({
+        model: openai("gpt-4o"),
+        prompt: req.body.message,
+    })
+    res.json({ text })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
+
